d13b6b4211bdab6a7fbbe6633d270a64082f4f1f
Oguzhan YAGCI <oyagci@student.42.fr>
2020-03-29 16:11:05 +0200

    Add assimp::Material::num_textures

--- src/scene/material.rs
+++ src/scene/material.rs
@@ -1,8 +1,14 @@
-use ffi::AiMaterial;
+use ffi::*;
 
 define_type_and_iterator_indirect! {
     /// Material type (not yet implemented)
     struct Material(&AiMaterial)
     /// Material iterator type.
     struct MaterialIter
+}
+
+impl<'a> Material<'a> {
+    pub fn num_textures(&self, kind: AiTextureType) -> u32 {
+        unsafe { aiGetMaterialTextureCount(self.0, kind) }
+    }
 }
