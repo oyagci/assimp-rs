367fd1bab168b5ed38f5a1f299d82d9f176f82ad
Oguzhan YAGCI <oyagci@student.42.fr>
2020-03-29 16:58:28 +0200

    Add assimp::Material::get_texture

--- src/scene/material.rs
+++ src/scene/material.rs
@@ -11,4 +11,21 @@ impl<'a> Material<'a> {
     pub fn num_textures(&self, kind: AiTextureType) -> u32 {
         unsafe { aiGetMaterialTextureCount(self.0, kind) }
     }
+
+    pub fn get_texture(&self, kind: AiTextureType, index: u32, name: &str) {
+        let mut aistr: AiString = From::from(name);
+        unsafe {
+            aiGetMaterialTexture(
+                self.0,
+                kind,
+                index,
+                &mut aistr,
+                std::ptr::null_mut(),
+                std::ptr::null_mut(),
+                std::ptr::null_mut(),
+                std::ptr::null_mut(),
+                std::ptr::null_mut(),
+                std::ptr::null_mut());
+        }
+    }
 }
